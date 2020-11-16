97a3f595f6c8e8cd95420c3039bee24db7e857b0
HP Clichy <dm@mirabel-sil.com>
2020-11-16 21:52:52 +0100

    Kiosque WIP (16/11-2152)

--- app/src/main/java/za/dams/kiosque/MainActivity.java
+++ app/src/main/java/za/dams/kiosque/MainActivity.java
@@ -1,5 +1,6 @@
 /*
 Logcat filter : ^(?:(?!eglCodecCommon).)*$
+https://stackoverflow.com/questions/24187728/sticky-immersive-mode-disabled-after-soft-keyboard-shown
  */
 
 package za.dams.kiosque;
@@ -20,6 +21,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
@@ -28,6 +30,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.Toolbar;
 
 
@@ -54,6 +57,8 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
         }
         setupKeyboardVisibilityListener() ;
         updateUI();
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     @Override
@@ -122,7 +127,7 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
             showSystemUI() ;
         }
     }
-    private void showSystemUI() {
+    private void showsSystemUI() {
         getWindow().getDecorView().setSystemUiVisibility(0);
         ActionBar actionBar = getActionBar();
         //actionBar.show();
@@ -181,6 +186,50 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
         }
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        // When the window loses focus (e.g. the action overflow is shown),
+        // cancel any pending hide action. When the window gains focus,
+        // hide the system UI.
+        if (hasFocus) {
+            Log.w("DAMS","Regain focus") ;
+            delayedHide(300);
+        } else {
+            mHideHandler.removeMessages(0);
+        }
+    }
 
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LOW_PROFILE |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+
+    private void showSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+    }
+
+    private final Handler mHideHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            hideSystemUI();
+        }
+    };
+
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeMessages(0);
+        mHideHandler.sendEmptyMessageDelayed(0, delayMillis);
+    }
 
 }
