b7c91b8a68b61f1dfe18f035ba5001212fefaf20
HP Clichy <dm@mirabel-sil.com>
2020-11-16 23:29:40 +0100

    Kiosque WIP (16/11-2329)

--- app/src/main/java/za/dams/kiosque/MainActivity.java
+++ app/src/main/java/za/dams/kiosque/MainActivity.java
@@ -37,6 +37,13 @@ import android.widget.Toolbar;
 public class MainActivity extends Activity
 implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackChangedListener {
 
+    private final Handler mUpdateUiHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            updateUI();
+        }
+    };
+
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,10 +62,12 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
             ft.commit();
             Log.w("DAMS","Adding fragment") ;
         }
-        setupKeyboardVisibilityListener() ;
-        updateUI();
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Restore Fullscreen on keyboard hide
+        setupKeyboardVisibilityListener() ;
+
+
+        updateUI();
     }
 
     @Override
@@ -116,23 +125,37 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
         updateUI();
 
     }
+
+
+    private void delayedUpdateUI(int delayMillis) {
+        mUpdateUiHandler.removeMessages(0);
+        if( delayMillis >= 0 ) {
+            mUpdateUiHandler.sendEmptyMessageDelayed(0, delayMillis);
+        }
+    }
     private void updateUI() {
         Fragment currentBackStackFragment = getFragmentManager().findFragmentByTag("visible_fragment");
         boolean isWebSession = (currentBackStackFragment != null && currentBackStackFragment instanceof SecondFragment) ;
 
 
         if( isWebSession ) {
-            hideSystemUi() ;
+            hideSystemUI() ;
         } else {
             showSystemUI() ;
         }
     }
-    private void showsSystemUI() {
+    private void showSystemUI() {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         getWindow().getDecorView().setSystemUiVisibility(0);
+
         ActionBar actionBar = getActionBar();
         //actionBar.show();
     }
-    private void hideSystemUi() {
+    private void hideSystemUI() {
+        // Prevent actionbar from disabling fullscreen
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
@@ -141,7 +164,9 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 // Hide the nav bar and status bar
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                // Low profile ?
+                |View.SYSTEM_UI_FLAG_LOW_PROFILE);
 
         ActionBar actionBar = getActionBar();
         //actionBar.hide();
@@ -175,14 +200,7 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
     public void onKeyboardVisibilityChanged(boolean visible) {
         Log.w("dams","keybord visible = "+visible) ;
         if( !visible ) {
-            final Handler handler = new Handler(Looper.getMainLooper());
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    updateUI() ;
-                }
-            }, 100);
-
+            delayedUpdateUI(300);
         }
     }
 
@@ -192,44 +210,14 @@ implements FirstFragment.OnButtonClickedListener, FragmentManager.OnBackStackCha
         // When the window loses focus (e.g. the action overflow is shown),
         // cancel any pending hide action. When the window gains focus,
         // hide the system UI.
+        // DM : re-hide everything when actionbar menu hides
         if (hasFocus) {
-            Log.w("DAMS","Regain focus") ;
-            delayedHide(300);
+            delayedUpdateUI(300);
         } else {
-            mHideHandler.removeMessages(0);
+            delayedUpdateUI(-1);
         }
     }
 
-    private void hideSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_LOW_PROFILE |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        );
-    }
 
-    private void showSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        );
-    }
-
-    private final Handler mHideHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            hideSystemUI();
-        }
-    };
-
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeMessages(0);
-        mHideHandler.sendEmptyMessageDelayed(0, delayMillis);
-    }
 
 }
