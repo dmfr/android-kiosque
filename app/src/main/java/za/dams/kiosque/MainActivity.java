1316fd229a51a834b3bf5610537e3ecc129d7752
HP Clichy <dm@mirabel-sil.com>
2021-01-21 19:11:15 +0100

    Kiosque WIP (21/01-1911, signature pad test)

--- app/src/main/java/za/dams/kiosque/MainActivity.java
+++ app/src/main/java/za/dams/kiosque/MainActivity.java
@@ -145,6 +145,9 @@ implements FirstFragment.OnButtonClickedListener
         if (id == R.id.action_zxing) {
             scanFragment();
         }
+        if (id == R.id.action_signature) {
+            signatureFragment();
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -158,6 +161,9 @@ implements FirstFragment.OnButtonClickedListener
         }
         if( fragment instanceof ScanFragment ) {
             ((ScanFragment) fragment).setListener(this);
+        }
+        if( fragment instanceof SignatureFragment ) {
+           // ((SignatureFragment) fragment).setListener(this);
         }
         if( fragment instanceof LinkListFragment ) {
             ((LinkListFragment) fragment).setLinkListActionListener(this);
@@ -345,6 +351,22 @@ implements FirstFragment.OnButtonClickedListener
 
         // Create and show the dialog.
         ScanFragment newFragment = ScanFragment.newInstance("str1","str2");
+        newFragment.show(ft, "dialog");
+    }
+
+    public void signatureFragment() {
+        // DialogFragment.show() will take care of adding the fragment
+        // in a transaction.  We also want to remove any currently showing
+        // dialog, so make our own transaction and take care of that here.
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        SignatureFragment newFragment = SignatureFragment.newInstance("str1","str2");
         newFragment.show(ft, "dialog");
     }
 
