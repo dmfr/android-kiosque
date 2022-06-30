b05ab93029cd1224773787e6c29b51b4a63ba2ac
you@example.com <you@example.com>
2022-06-30 20:50:23 +0200

    TestActivity 29/06, old Theme, deprecated ActionBar.Tab

--- app/src/main/java/za/dams/kiosque/TestActivity.java
+++ app/src/main/java/za/dams/kiosque/TestActivity.java
@@ -1,17 +1,68 @@
 package za.dams.kiosque;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends Activity implements ActionBar.TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        /*
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setActionBar(toolbar);
+        setActionBar(toolbar);*/
+
+        setupTabs();
+    }
+    private void setupTabs() {
+        ActionBar actionBar ;
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(true);
+
+        ActionBar.Tab tab1 = actionBar
+                .newTab()
+                .setText("First")
+                .setIcon(android.R.drawable.ic_delete)
+                .setTabListener(this);
+
+        actionBar.addTab(tab1);
+        actionBar.selectTab(tab1);
+
+        ActionBar.Tab tab2 = actionBar
+                .newTab()
+                .setText("Second")
+                .setIcon(android.R.drawable.ic_menu_save)
+                .setTabListener(this);
+
+        actionBar.addTab(tab2);
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 }
