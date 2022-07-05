384c11631735b8f5b363c3e04fb28e90eb8b4c1a
you@example.com <you@example.com>
2022-07-05 17:04:26 +0200

    TestActivity 29/06, ViewPager adapter

--- app/src/main/java/za/dams/kiosque/TestActivity.java
+++ app/src/main/java/za/dams/kiosque/TestActivity.java
@@ -2,12 +2,56 @@ package za.dams.kiosque;
 
 
 
+
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
-public class TestActivity extends Activity {
+import androidx.fragment.app.FragmentActivity ;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+
+import com.google.android.material.tabs.TabLayout;
+
+public class TestActivity extends FragmentActivity {
+
+
+    private class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 3;
+        private String tabTitles[] = new String[] { "Scan", "Photos", "Signature" };
+        private Context context;
+
+        public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return DummyFragment.newInstance("pouet","pouet");
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Generate title based on item position
+            return tabTitles[position];
+        }
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +60,15 @@ public class TestActivity extends Activity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setActionBar(toolbar);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
+                TestActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 
