57b5f5b3c3af13c93aeee9b9e4d4bd1cae6aa1b8
you@example.com <you@example.com>
2022-07-05 16:51:23 +0200

    TestActivity 29/06, material Theme

--- app/src/main/java/za/dams/kiosque/TestActivity.java
+++ app/src/main/java/za/dams/kiosque/TestActivity.java
@@ -2,46 +2,20 @@ package za.dams.kiosque;
 
 
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
-public class TestActivity extends Activity implements ActionBar.TabListener {
+public class TestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        /*
+
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setActionBar(toolbar);*/
-
-        setupTabs();
-    }
-    private void setupTabs() {
-        ActionBar actionBar ;
-        actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setDisplayShowTitleEnabled(true);
-
-        ActionBar.Tab tab1 = actionBar
-                .newTab()
-                .setText("First")
-                .setIcon(android.R.drawable.ic_delete)
-                .setTabListener(this);
-
-        actionBar.addTab(tab1);
-        actionBar.selectTab(tab1);
-
-        ActionBar.Tab tab2 = actionBar
-                .newTab()
-                .setText("Second")
-                .setIcon(android.R.drawable.ic_menu_save)
-                .setTabListener(this);
-
-        actionBar.addTab(tab2);
+        setActionBar(toolbar);
     }
 
 
@@ -51,18 +25,6 @@ public class TestActivity extends Activity implements ActionBar.TabListener {
 
 
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
-    }
 
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
 }
