68df4ff11c03ad5431a84e155553eb4e31f7c303
you@example.com <you@example.com>
2022-08-23 01:09:05 +0200

    TestActivity 22/08-2359 (TestCameraFragment cameraX)

--- app/src/main/java/za/dams/kiosque/TestCameraFragment.java
+++ app/src/main/java/za/dams/kiosque/TestCameraFragment.java
@@ -1,17 +1,34 @@
 package za.dams.kiosque;
 
+import androidx.annotation.Nullable;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.content.Context;
 import android.graphics.Point;
 import android.os.Bundle;
 
 import android.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +39,21 @@ public class TestCameraFragment extends DialogFragment {
     public interface CameraListener {
         void onCameraResult(boolean photoTaken);
     }
-
     private CameraListener mListener ;
 
+    private static final class MainThreadExecutor implements Executor {
+        static final Executor INSTANCE = new MainThreadExecutor();
+
+        private final Handler handler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(Runnable r) {
+            handler.post(r);
+        }
+    }
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView ;
 
     public TestCameraFragment() {
         // Required empty public constructor
@@ -51,10 +80,40 @@ public class TestCameraFragment extends DialogFragment {
 
         View rootView = inflater.inflate(R.layout.fragment_test_camera, container, false);
 
-
+        previewView = rootView.findViewById(R.id.preview_view) ;
 
         return rootView;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(getActivity());
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, MainThreadExecutor.INSTANCE);
+    }
+
+    void bindPreview(ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+    }
+
     @Override
     public void onResume() {
         // Set the width of the dialog proportional to 90% of the screen width
@@ -73,4 +132,9 @@ public class TestCameraFragment extends DialogFragment {
         super.onResume();
         //barcodeView.resume();
     }
+
+
+
+
+
 }
